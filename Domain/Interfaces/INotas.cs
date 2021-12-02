05ff3ed25fa1ffe5b1186ab825654c3244a1cb10
alvarosolano2003 <85872308+alvarosolano2003@users.noreply.github.com>
2021-12-02 09:57:36 -0600

    Add files via upload

---
+++ Domain/Interfaces/INotas.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface INotas : IModel<Nota>
+    {
+        List<Nota> ClasificateNota(Estudiante est);
+        decimal CalculatePromedio(List<Nota> nota);
+        List<Estudiante> MejoresPromedio(List<Estudiante> est, decimal[] promedios);
+    }
+}
---
+++ Infraestructure/Repositories/NotaRepository.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public class NotaRepository : BaseRepository<Nota>, INotas
+    {
+        public decimal CalculatePromedio(List<Nota> nota)
+        {
+            decimal promedio = 0;
+
+            foreach (Nota e in nota)
+            {
+                promedio += e.NotaFinal;
+            }
+
+            return Math.Round(promedio);
+        }
+        public List<Nota> ClasificateNota(Estudiante est)
+        {
+            return data.Where(x => x.Estudiante == est).ToList();
+        }
+        public List<Estudiante> MejoresPromedio(List<Estudiante> est, decimal[] promedios)
+        {
+            Array.Sort(promedios);
+            List<Nota> estNotas = new List<Nota>();
+            List<Estudiante> bestAverages = new List<Estudiante>();
+            decimal promAux = 0;
+
+            for (int i = 0; i > est.Count; i++)
+            {
+                estNotas = ClasificateNota(est[i]);
+                promAux = CalculatePromedio(estNotas);
+
+                if (promAux == promedios[1])
+                {
+                    bestAverages.Add(est[i]);
+                }
+                else if (promAux == promedios[2])
+                {
+                    bestAverages.Add(est[i]);
+                }
+                else if (promAux == promedios[2])
+                {
+                    bestAverages.Add(est[i]);
+                }
+            }
+
+            return bestAverages;
+        }
+    }
+}
