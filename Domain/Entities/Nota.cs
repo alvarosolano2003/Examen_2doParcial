259ca600c02d6a1e33c134d7efb667c89480e44a
MoisesNunez435 <85852193+MoisesNunez435@users.noreply.github.com>
2021-12-02 08:33:42 -0600

    Add files via upload
    Agregando Domain

---
+++ Domain/Entities/Nota.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Nota
+    {
+        public Asignatura Asignatura { get; set; }
+        public Estudiante Estudiante { get; set; }
+        public int Sistematico { get; set; }
+        public int IParcial { get; set; }
+        public int IIParcial { get; set; }
+        public int TareaCurso { get; set; }
+        public int NotaFinal { get; set; }
+    }
+}
