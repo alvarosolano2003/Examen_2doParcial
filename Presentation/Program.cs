a6830999995762a598a31367cc4298d800b2e1d4
MoisesNunez435 <85852193+MoisesNunez435@users.noreply.github.com>
2021-12-02 10:08:27 -0600

    Add files via upload

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -25,7 +25,7 @@ namespace Presentation
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmEstudiante());
         }
     }
 }
